3d66c88984009f393f8788e7cd8366f87524136b
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2024-01-13 13:19:59 +0800

    :bug: fix image pattern

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -36,7 +36,7 @@ buildConfig {
     buildConfigField("TOOL_API_URL", "https://bs-helper.api.ktlab.io")
     buildConfigField("FEEDBACK_URL", "https://bs-helper.api.ktlab.io/feedback")
     buildConfigField("BS_IMG_PROXY", "https://bs-helper.img.ktlab.io")
-    buildConfigField("List<kotlin.text.Regex>","BS_IMG_PATTERNS", arrayOf("Regex(\"https://eu.cdn.beatsaver.com\")"))
+    buildConfigField("List<kotlin.text.Regex>","BS_IMG_PATTERNS", arrayOf("Regex(\"https://(.+\\\\.)?cdn\\\\.beatsaver\\\\.com\")"))
     buildConfigField("BS_API_URL", "https://api.beatsaver.com")
 }
 
